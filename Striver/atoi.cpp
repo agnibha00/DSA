e3e2c9e7f2164fc1a255a4e73fc51d9c2322fd72
agnibha00 <itsagnibhakundu@gmail.com>
2026-02-23 20:56:27 +0530

    upload

---
+++ Striver/atoi.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int solve (string s , int i , long long result, int sign)
+{
+    if(i<s.size() || !isdigit(s[i])){
+        return sign * result;
+    }
+}
+int main() {
+    
+    return 0;
+}
