0a80c5c23729f16d942fb4e19bf93387852a6b3b
Agnibha <itsagnibhakundu@gmail.com>
2026-02-25 14:00:10 +0530

    upload

--- Striver/Resursion/atoi.cpp
+++ Striver/Resursion/atoi.cpp
@@ -1,45 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-// -2144 jkshgkajhv32
-
-int solve(string s, long long result, int i, int sign){
-    if(i>=s.size() || s[i]<'0' || s[i]>'9'){
-        return sign* result;
-    }
-
-    int digit= s[i]-'0';
-    if(result>(LLONG_MAX-digit)/10){
-        return sign==1? INT_MAX:INT_MIN ;
-    }
-
-    result = result *10+digit;
-    return solve(s,result, i+1, sign);
-}
-
-int main()
+int solve (string s , int i , long long result, int sign)
 {
-    string s;
-    cout<<"enter the string ";
-    cin>>s;
-
-    int i =0;
-    int sign =1;
-
-
-    while(i<s.size() && s[i]==' ') i++;
-
-    if(s[i]=='-') {
-        sign = -1;
-        i++;
+    if(i<s.size() || !isdigit(s[i])){
+        return sign * result;
     }
-    else if(s[i]=='+') {
-        i++;
-    }
-
+}
+int main() {
     
-    int sol= solve(s,0,i,sign);
-   
-    cout<< sol;
     return 0;
 }
