1b2cd2fee8bd782a0f53b2159627bb67d709cc13
Agnibha Kundu <120159252+agnibha00@users.noreply.github.com>
2026-07-01 22:03:15 +0530

    Time: 85 ms (11.72%), Space: 113.4 MB (5.45%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        for(auto it =mp.begin();it!=mp.end();it++){
-            if(it->second>1){
+        for(auto it :mp){
+            if(it.second>1){
                 return true;
             }
         }
