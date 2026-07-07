48706206f44da2c7c6707532d4771a640486fdea
Agnibha Kundu <120159252+agnibha00@users.noreply.github.com>
2026-07-07 19:09:43 +0530

    Time: 75 ms (17.23%), Space: 52.2 MB (5.19%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        set<int> s;
+
+        for(auto it:arr){
+            s.insert(it);
+        }
+
+        unordered_map<int,int>mp;
+        int r=1;
+
+        for(auto it:s){
+            mp[it]=r;
+            r++;
+        }
+
+        vector<int>ans;
+        for(auto it:arr){
+            ans.push_back(mp[it]);
+        }
+        return ans;
+    }
+};
